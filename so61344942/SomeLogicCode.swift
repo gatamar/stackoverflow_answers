3d76b11766380f7bb8a81efe306244e6d0a346e0
Olha Pavliuk <olia.pavliuk@gmail.com>
2020-05-03 18:33:49 +0300

    answered https://stackoverflow.com/questions/61344942/xctest-for-a-mac-command-line-application

---
+++ so61344942/SomeLogicCode.swift
@@ -0,0 +1,9 @@
+//
+//  SomeLogicCode.swift
+//  so61344942
+//
+//  Created by Olha Pavliuk on 03.05.2020.
+//  Copyright © 2020 Olha Pavliuk. All rights reserved.
+//
+
+import Foundation
---
+++ so61344942/so61344942/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  so61344942
+//
+//  Created by Olha Pavliuk on 03.05.2020.
+//  Copyright © 2020 Olha Pavliuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
